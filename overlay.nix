825b0d92c7d3da69e08d82aeb35b95371b2491eb
ryleu <69326171+ryleu@users.noreply.github.com>
2025-10-21 11:23:06 -0500

    remove all instances of ros2_interfaces_pkg
    - should've used ag the first time :sob:

--- overlay.nix
+++ overlay.nix
@@ -1,5 +1,4 @@
 final: prev:
 {
   astra-msgs = final.callPackage ././package.nix {};
-  ros2-interfaces-pkg = final.callPackage ././install/ros2_interfaces_pkg/share/ros2_interfaces_pkg/package.nix {};
 }
